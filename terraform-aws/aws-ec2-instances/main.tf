a55fe1be3a82b09e5885c6b70daba3b9065c0b35
nagarajukusa24 <nagarajukusa24@gmail.com>
2023-07-25 06:57:19 +0530

    Setting up Harness pipeline

--- terraform-aws/aws-ec2-instances/main.tf
+++ terraform-aws/aws-ec2-instances/main.tf
@@ -1,3 +1,24 @@
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.1.7"
+
+  account_id = "_U_L7us0TlGrA1298QAJZw"
+  delegate_token = "NDJlN2EzODc0NTMxNmI5OTM5YTNkNGZlOWFhMzhmZWI="
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io/gratis"
+  delegate_image = "harness/delegate:23.07.79904"
+  replicas = 1
+  upgrader_enabled = false
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 terraform {
   required_version = ">= 1.0"
 
